f643d8bc9615f3b6ffb5ad97ce7ff9dc728608da
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-06-29 09:38:29 +0530

    fix: tighten org finder review blockers
    Signed-off-by: Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>

---
+++ tailwind.contributing.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./contributing.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#9d4300',
+        'primary-container': '#f97316',
+      },
+    },
+  },
+};
